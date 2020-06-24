0b7d75809369fbb5ee584c0564cc61f5a4eb5b2c
DiptoChakrabarty <diptochuck123@gmail.com>
2020-06-24 21:00:45 +0530

    inputs

--- user.py
+++ user.py
@@ -8,8 +8,10 @@ client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 # Connect/ssh to an instance
 try:
-    # Here 'ubuntu' is user name and 'instance_ip' is public IP of EC2
-    client.connect(hostname=instance_ip, username="ubuntu", pkey=key)
+    # Provide username and   public IP of EC2
+    instance_ip = input("Add the instance Ip address : ")
+    username = input("What is the username : ")
+    client.connect(hostname=instance_ip, username=username, pkey=key)
 
     # Execute a command(cmd) after connecting/ssh to an instance
     stdin, stdout, stderr = client.exec_command(cmd)
